9eb3dee1e67b7c47c7b304244cef025ff435397a
Metod Ribič <metod.ribic@gmail.com>
2016-04-14 22:56:13 +0200

    bugfix urls.py

--- wsgi/backend/tpo/urls.py
+++ wsgi/backend/tpo/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 
-from tpo.views import UporabnikiViewSet, PreglediViewSet, PostaViewSet, AmbulantaViewSet, UstanovaViewSet, ZdravnikViewSet, \git
+from tpo.views import UporabnikiViewSet, PreglediViewSet, PostaViewSet, AmbulantaViewSet, UstanovaViewSet, ZdravnikViewSet, \
     OsebjeViewSet, MeritevViewSet, DietaViewSet, BolezniViewSet, ZdraviloViewSet, RolesViewSet, login, NavodiloDietaViewSet, changePassword, \
     ZdravnikUporabnikiViewSet
 
