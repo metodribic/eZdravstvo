a2d3f267e84f780ddd14ecbd57d2bf8635353a16
Metod Ribič <metod.ribic@gmail.com>
2016-05-07 11:18:55 +0200

    Added new database dump

--- frontend/app/scripts/controllers/index.js
+++ frontend/app/scripts/controllers/index.js
@@ -1,7 +1,7 @@
 'use strict()';
 
 angular.module('tpo')
- .controller('IndexCtrl', ['$scope', 'Uporabniki',function ($scope, Uporabniki) {
+ .controller('IndexCtrl', ['$scope', 'RegistracijaPacient', 'Uporabniki',function ($scope, RegistracijaPacient, Uporabniki) {
    $scope.ustvariOskrbovanca = function(){
      console.log('dodaj Oskrbovanca');
    };
