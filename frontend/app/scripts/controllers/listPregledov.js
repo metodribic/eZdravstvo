b9b36f899c50894b151db668d4865a6a9c905946
Metod Ribič <metod.ribic@gmail.com>
2016-05-14 14:51:09 +0200

    Fixed empty pregledList

--- frontend/app/scripts/controllers/listPregledov.js
+++ frontend/app/scripts/controllers/listPregledov.js
@@ -1,16 +1,16 @@
 /**
  * Created by jk on 06/04/2016.
  */
-'use strict';
+'use strict()';
 
 /* Controler za podrobni pogled pregleda*/
 
 angular.module('tpo')
   .controller('ListPregledovCtrl', ['$scope','Pregled', function ($scope, Pregled) {
         //pridobi preglede vseh zdravnikovih pacientov
-      
+
         Pregled.query().$promise.then(function(response){
         $scope.pregledi = response;
-        // console.log($scope.pregledi);
+        console.log($scope.pregledi.length);
     });
   }]);
