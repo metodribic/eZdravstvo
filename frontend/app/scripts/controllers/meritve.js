a58e7baac657512a8a3420a983ddf6ae534816ad
Metod Ribič <metod.ribic@gmail.com>
2016-05-31 21:39:56 +0200

    Updated dodajanje meritve, checking if inputs are empty and if they are in allowed range

--- frontend/app/scripts/controllers/meritve.js
+++ frontend/app/scripts/controllers/meritve.js
@@ -4,6 +4,9 @@ angular.module('tpo')
   .controller('MeritveCtrl', ['$scope','AuthService', '$state', '$rootScope','Meritve','Notification','VrednostiMeritevSeznam','$interval', function ($scope, AuthService, $state, $rootScope, Meritve, Notification,VrednostiMeritevSeznam, $interval) {
 
     $scope.vrednostMeritve = null;
+    $scope.vrednostMeritveSistolicni = null;
+    $scope.vrednostMeritveDiastolicni = null;
+    $scope.vrednostMeritveUtrip = null;
     // update clock
     $scope.datum = moment().format("DD.MM.YYYY, HH:mm");
     $interval(function () {
@@ -15,6 +18,19 @@ angular.module('tpo')
       $scope.meritve = response;
     });
 
+    // get all types of meritve
+    VrednostiMeritevSeznam.query().$promise.then(function(response) {
+      $scope.vrednosti_meritev = response;
+
+      for (var el in response){
+        // tega rabiš posebi, da lahko pri pritisku preveriš če je input v mejah...
+        if(response[el].tip === "Srčni pritisk"){
+          $scope.vrednostiZaUtrip = response[el];
+          break;
+        }
+      }
+    });
+
     // delete meritev
     $scope.deleteMeritev = function(id){
       Meritve.delete({meritevId: id}).$promise.then(function(response){
@@ -22,11 +38,6 @@ angular.module('tpo')
         $scope.reloadState();
       });
     };
-
-    // get all types of meritve
-    VrednostiMeritevSeznam.query().$promise.then(function(response) {
-      $scope.vrednosti_meritev = response;
-    });
 
     // select meritev tip
     $scope.izberiMeritev = function(item){
@@ -39,45 +50,83 @@ angular.module('tpo')
       $state.go($state.current, {}, {reload: true});
     };
 
+    // reset form
+    $scope.resetForm = function(){
+      $scope.vrednostMeritve = null;
+      $scope.vrednostMeritveSistolicni = null;
+      $scope.vrednostMeritveDiastolicni = null;
+      $scope.vrednostMeritveUtrip = null;
+    };
+
     $scope.saveMeritev = function(){
-      if($scope.izbranaMeritev.tip == 'Krvni pritisk'){
-        console.log('Test');
+      if($scope.izbranaMeritev === undefined){
+        Notification.warning('Najprej izberite tip meritve!');
+      }
+      else if($scope.izbranaMeritev.tip == 'Krvni pritisk'){
+
+        // first check if all field are filled
+        // prvo preveri če so null, potem pa če niso, preveri še če so dolžine inputov daljše kot 0,
+        // ker če uporabnik najrpej nekaj vpiše pa potem zbriše ni null, ampak ga vseeno ne smeš spustit naprej
+        if($scope.vrednostMeritveSistolicni === null || $scope.vrednostMeritveDiastolicni === null || $scope.vrednostMeritveUtrip === null ||
+          $scope.vrednostMeritveSistolicni.length < 0 || $scope.vrednostMeritveDiastolicni.length < 0 || $scope.vrednostMeritveUtrip.length < 0){
+          Notification.warning('Za nadaljevanje vnesite vrednosti meritve!');
+        }
+        // preveri veljavnost vpisanih podatkov
+        else if(!checkValidInput()){
+          Notification.warning({message: 'Mogoče vrednosti so: <br> pritisk: od '+$scope.izbranaMeritev.nemogoce_min+' do '+$scope.izbranaMeritev.nemogoce_max+'<br> utrip: od '+$scope.vrednostiZaUtrip.nemogoce_min+' do '+$scope.vrednostiZaUtrip.nemogoce_max, title: '<b>Vrednosti meritve so zunaj dovoljenih vrednosti!</b>'});
+        }
+        else{
+          console.log('Shrani krvni pritisk');
+        }
       }
       else{
-        var novaMeritev = new Meritve();
-        novaMeritev.tip_meritve = $scope.izbranaMeritev;
-        novaMeritev.vrednost_meritve = $scope.vrednostMeritve;
-        novaMeritev.uporabnik = $rootScope.uporabnik;
-        novaMeritev.pregled = null;
-        novaMeritev.datum = moment().format("YYYY-MM-DD");
-        novaMeritev.$save(function(response){
-          console.log(response);
-        });
+        // preveri če je polje vrednost izpoljeno
+        if($scope.vrednostMeritve === null || $scope.vrednostMeritve.length === 0){
+          Notification.warning('Za nadaljevanje vnesite vrednost meritve!');
+        }
+        // preveri veljavnost vpisanih podatkov
+        else if(!checkValidInput()){
+          Notification.warning({message: 'Mogoče vrednosti so od '+$scope.izbranaMeritev.nemogoce_min+' do '+$scope.izbranaMeritev.nemogoce_max, title: '<b>Vrednosti meritve so zunaj dovoljenih vrednosti!</b>'});
+        }
+        // če je potem shrani meritev
+        else{
+          var novaMeritev = new Meritve();
+          novaMeritev.tip_meritve = $scope.izbranaMeritev;
+          novaMeritev.vrednost_meritve = $scope.vrednostMeritve;
+          novaMeritev.uporabnik = $rootScope.uporabnik;
+          novaMeritev.pregled = null;
+          novaMeritev.datum = moment().format("YYYY-MM-DD");
+          novaMeritev.$save(function(response){
+            console.log(response);
+          });
+        }
       }
-
-    //   switch($scope.izbranaMeritev.tip) {
-    //     case 'Telesna temperatura':
-    //         // save telesna temperatura
-    //         console.log('Saving telesna temperatura');
-    //         break;
-    //     case 'Glukoza':
-    //         // save glukoza
-    //         console.log('Saving glukoza');
-    //         break;
-    //     case 'ITM':
-    //         // save ITM
-    //         console.log('Saving itm');
-    //         break;
-    //     case 'Srčni pritisk':
-    //         // save utrip
-    //         console.log('Saving utrip');
-    //         break;
-    //     case 'Krvni pritisk':
-    //         // save pritisk
-    //         console.log('Saving pritisk + utrip');
-    //         break;
-    //   }
     };
+
+    // preveri če je vpisana vrednsot znotraj meja
+    function checkValidInput(){
+      if($scope.izbranaMeritev.tip == 'Krvni pritisk'){
+        var defaultMin = $scope.izbranaMeritev.nemogoce_min;
+        var defaultMax = $scope.izbranaMeritev.nemogoce_max;
+
+        if(parseInt($scope.vrednostMeritveSistolicni) >= defaultMin && parseInt($scope.vrednostMeritveSistolicni <= defaultMax) &&
+        parseInt($scope.vrednostMeritveDiastolicni) >= defaultMin && parseInt($scope.vrednostMeritveDiastolicni <= defaultMax)){
+          // preveri še utrip
+          if(parseInt($scope.vrednostMeritveUtrip) >= parseInt($scope.vrednostiZaUtrip.nemogoce_min) &&
+          parseInt($scope.vrednostMeritveUtrip) <= parseInt($scope.vrednostiZaUtrip.nemogoce_max))
+            return true;
+        }
+        else
+          return false;
+      }
+      else{
+        if(parseInt($scope.vrednost_meritve) >= parseInt($scope.izbranaMeritev.nemogoce_min) &&
+        parseInt($scope.vrednost_meritve) <= parseInt($scope.izbranaMeritev.nemogoce_max))
+          return true;
+        else
+          return false;
+      }
+    }
 
 
   }]);
