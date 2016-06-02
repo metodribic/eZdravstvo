65e8f99aad8964257ae9fb9182d3302ca94d25b8
Rok Mirt <rm9289@student.uni-lj.si>
2016-06-02 23:01:40 +0200

    Fix for charts

--- frontend/app/scripts/controllers/meritevPodrobno.js
+++ frontend/app/scripts/controllers/meritevPodrobno.js
@@ -6,35 +6,7 @@ angular.module('tpo')
 
     Meritve.get({ meritevId:$stateParams.id}).$promise.then(function(response){
       $scope.meritev = response;
-
-      $scope.data = [{
-    		key: "Graf za trenutno meritev",
-    		values: [{"label": "Trenutna meritev", "value": $scope.meritev.vrednost_meritve}]
-    	}];
-
-      if($scope.meritev.tip_meritve.tip === "Krvni pritisk") {
-          scope.config.visible = true;
-          var spodnji = parseFloat($scope.meritev.vrednost_meritve.split("/")[0]);
-          var zgornji = parseFloat($scope.meritev.vrednost_meritve.split("/")[1]);
-          $scope.data = [{
-              key: "Graf za trenutno meritev",
-              values: [{"label": "Trenutna meritev", "value": spodnji}]
-          }]
-          
-          $scope.data2 = [{
-              key: "Graf za trenutno meritev",
-              values: [{"label": "Trenutna meritev", "value": zgornji}]
-          }]
-
-      } else {
-          $scope.data = [{
-              key: "Graf za trenutno meritev",
-              values: [{"label": "Trenutna meritev", "value": $scope.meritev.vrednost_meritve}]
-          }]
-      }
-
     });
-
 
     $scope.chart = {'start': moment(), 'end': moment()};
 
@@ -44,20 +16,36 @@ angular.module('tpo')
         tipMeritveId = $scope.meritev.tip_meritve.id;
         Meritve.query({tipMeritveId: tipMeritveId, startDate: startDate.format('YYYY-MM-DD'),
             endDate: endDate.format('YYYY-MM-DD')}).$promise.then(function(response) {
-                data = [{key: "Graf meritev od " + startDate.format('DD.MM.YYYY') + ' - ' +
-                    endDate.format('DD.MM.YYYY'), values: []}];
+                data = [{values: []}]
+                scope.config.visible = true;
+
                 for(var i=0; i<response.length; i++) {
+                        console.log(response[i].datum)
+                    if(i === 0)
+                        data[0].key = response[i].tip_meritve.tip;
                     if(response[i].tip_meritve.tip === "Krvni pritisk") {
-                        scope.config.visible = true;
+                        if(data.length < 2) {
+                            data.push({values:[], key: "Sistolični krvni pritisk"});
+                            data[0].key = "Diastolični krvni pritisk";
+                        }
                         var spodnji = parseFloat(response[i].vrednost_meritve.split("/")[0]);
                         var zgornji = parseFloat(response[i].vrednost_meritve.split("/")[1]);
-                        data[0].values.push({"label": moment(response[i].datum, 'YYYY-MM-DD').format('DD.MM.YYYY'),
-                                     "value": spodnji});
-                        data2[0].values.push({"label": moment(response[i].datum, 'YYYY-MM-DD').format('DD.MM.YYYY'),
-                                     "value": zgornji});
+
+                        data[0].values.push({
+							x: moment(response[i].datum, 'YYYY-MM-DD ').valueOf(),
+							y: spodnji
+                        });
+
+                        data[1].values.push({
+							x: moment(response[i].datum, 'YYYY-MM-DD ').valueOf(),
+							y: zgornji
+                        });
+
                     } else {
-                        data[0].values.push({"label": moment(response[i].datum, 'YYYY-MM-DD').format('DD.MM.YYYY'),
-                                     "value": parseFloat(response[i].vrednost_meritve)});
+                        data[0].values.push({
+							x: moment(response[i].datum, 'YYYY-MM-DD ').valueOf(),
+							y: parseFloat(response[i].vrednost_meritve),
+                        });
                     }
                 }
                 $scope.data = data;
@@ -66,44 +54,36 @@ angular.module('tpo')
             });
     };
 
-	$scope.data = [{
-		key: "",
-		values: []
-	}]
-    
-    $scope.data2 = [{
-		key: "",
-		values: []
-	}]
-
-
     $scope.options = {
-        chart: {
-            type: 'discreteBarChart',
-            height: 450,
-            margin : {
-                top: 20,
-                right: 20,
-                bottom: 60,
-                left: 55
-            },
-            x: function(d){ return d.label; },
-            y: function(d){ return d.value; },
-            showValues: true,
-            valueFormat: function(d){
-                return d3.format(',.4f')(d);
-            },
-            transitionDuration: 500,
-            xAxis: {
-                axisLabel: 'Meritve'
-            },
-            yAxis: {
-                axisLabel: 'Vrednost meritve',
-                axisLabelDistance: 30
+            chart: {
+                type: 'scatterChart',
+                height: 450,
+                color: d3.scale.category10().range(),
+                scatter: {
+                    onlyCircles: true
+                },
+                showDistX: true,
+                showDistY: true,
+                tooltipContent: function(key) {
+                    return '<h3>' + key + '</h3>';
+                },
+                duration: 350,
+                xAxis: {
+                    axisLabel: 'Datum',
+                    tickFormat: function(d){
+                        return d3.time.format('%d.%m.%Y')(new Date(d));
+                    },
+                    axisLabelDistance: 30
+                },
+                yAxis: {
+                    axisLabel: 'Y Axis',
+                    tickFormat: function(d){
+                        return d3.format('.02f')(d);
+                    },
+                    axisLabelDistance: 30
+                }
             }
-        }
-    };
-
+        };
     $scope.config = {
         visible: false
     }
