252a794aa4b099fedebee54e291d7c3d03f7f2e5
Metod Ribič <metod.ribic@gmail.com>
2016-06-09 16:38:54 +0200

    Updated holesterol in podrobniPregeldi - just bold

--- frontend/app/scripts/controllers/pregledPodrobno.js
+++ frontend/app/scripts/controllers/pregledPodrobno.js
@@ -18,14 +18,19 @@ angular.module('tpo')
 
       Meritve.query({pregledId: pregledID}).$promise.then(function(response) {
             $scope.meritve = response;
+
+            // boldaj samo rezultate, holesterol ima zraven še text
             for(i=0; i<$scope.meritve.length; i++) {
                 var m = $scope.meritve;
                 if(m[i].tip_meritve.tip === "Holesterol") {
                     var vrednosti = m[i].vrednost_meritve.split("/");
-                    m[i].vrednost_meritve = "<br/> Normalna: " + vrednosti[0] + "<br/> LDL: " + 
-                        vrednosti[1] + "<br /> HDL: " + vrednosti[2];
+                    m[i].vrednost_meritve = "<b>Normalni: " + vrednosti[0] + ", LDL: " + vrednosti[1] + ", HDL:" + vrednosti[2]+'</b>';
+                    // m[i].vrednost_meritve = "<b>" + vrednosti[0] + "/" + vrednosti[1] + "/" + vrednosti[2]+'</b> <i>(normalni/LDL/HDL)</i>';
+                }
+                else {
+                  m[i].vrednost_meritve = '<b>' + m[i].vrednost_meritve + '</b>'
                 }
             }
       });
-      
+
   }]);
